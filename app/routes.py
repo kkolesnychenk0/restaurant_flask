28a291db8a23b4f41c851be4d72cee06452b9522
Kate Kolesnychenko <kkolesnychenk00@gmail.com>
2022-10-03 16:32:09 +0300

    change menu page

--- app/routes.py
+++ app/routes.py
@@ -10,9 +10,10 @@ from app.email import send_email,send_password_reset_email
 
 
 def sum_order():
-    price_total = sum([list_item['price'] for list_item in session['card']])
-    quantity_total = sum([list_item['quantity'] for list_item in session['card']])
-    return price_total, quantity_total
+    if 'card'  in session:
+        price_total = sum([list_item['price'] for list_item in session['card']])
+        quantity_total = sum([list_item['quantity'] for list_item in session['card']])
+        return price_total, quantity_total
 
 
 @app.route('/',methods=['GET'])
@@ -48,13 +49,11 @@ def index():
                 session['card'].append({'id': form.code.data, 'name': form.name.data, 'quantity': 1,
                                         'price': form.price.data})
             session.modified = True
-    #price_total, quantity_total = sum_order()
-    price_total = sum([list_item['price'] for list_item in session['card']])
-    quantity_total = sum([list_item['quantity'] for list_item in session['card']])
+    price_total, quantity_total = sum_order()
 
     return render_template('index.html', title='Menu', products=products, len=len(products),
                            product_group=product_group, form=form,
-                           price_total=price_total, quantity_total=quantity_total,product_order=product_order)
+                           price_total=price_total, quantity_total=quantity_total)
 
 
 @app.route('/make_order', methods=['GET', 'POST'])
