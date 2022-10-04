4773b13f2465aff7441d67e3ef6374b66bdb45ab
Kate Kolesnychenko <kkolesnychenk00@gmail.com>
2022-10-04 12:51:47 +0300

    add block about company

--- config.py
+++ config.py
@@ -7,8 +7,6 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
         'postgres://', 'postgresql://') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    #SQLALCHEMY_DATABASE_URI = 'postgresql://yoguupsymkqcab:45e0edd4229991c42dbe18ee7386be68b0673abecc9203e51f' \
-    #                          '5f408c052aa6e0@ec2-54-155-110-181.eu-west-1.compute.amazonaws.com:5432/d25fdvi0lvl60f'
     #signal to app about changes in db
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.mailtrap.io'
